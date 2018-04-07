23bdb08f38caef6fde0c674d8b1a29bf2437e069
Jason <jason.r.mcneilly@gmail.com>
2018-04-07 15:12:17 -0400

    added initial MVC pattern for ToDoItems

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreToDo.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get items from the db
+
+            //Pass items to a model
+
+            //Render a view with the model data
+            return View();
+        }
+    }
+}
